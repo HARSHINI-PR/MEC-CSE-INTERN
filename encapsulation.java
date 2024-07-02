6e4656620159e0b53654efbffd2ff6cdb59c546f
HARSHINI-PR <149145601+HARSHINI-PR@users.noreply.github.com>
2024-07-02 21:52:14 +0530

    Update encapsulation.java

--- encapsulation.java
+++ encapsulation.java
@@ -40,20 +40,20 @@ class students {
 public class encapsulation{
     public static void main(String[] args) {
         students ss= new students();
-        ss.setstdid(501);
-        ss.setstdname("Gowthami Priya");
-        ss.setcollegename("TCS");
-        ss.setplace("Chennai");
+        ss.setstdid(084);
+        ss.setstdname("Harshini");
+        ss.setcollegename("MEC");
+        ss.setplace("Rasipuram");
 
 
         students ss1= new students();
-        ss1.setstdid(502);
-        ss1.setstdname("Annamalai");
-        ss1.setcollegename("ZEALOUS");
-        ss1.setplace("Salem");
+        ss1.setstdid(117);
+        ss1.setstdname("Nandhana");
+        ss1.setcollegename("KSR");
+        ss1.setplace("Harur");
 
-        System.out.println(ss.getstdid()+"\n"+ss.getstdname()+"\n"+ss.getcollegename()+"\n"+ss.getLocation());
-        System.out.println(ss1.getstdid()+"\n"+ss1.getstdname()+"\n"+ss1.getcollegename()+"\n"+ss1.getLocation());
+        System.out.println(ss.getstdid()+"\n"+ss.getstdname()+"\n"+ss.getcollegename()+"\n"+ss.getplace());
+        System.out.println(ss1.getstdid()+"\n"+ss1.getstdname()+"\n"+ss1.getcollegename()+"\n"+ss1.getplace());
 
 
     }
